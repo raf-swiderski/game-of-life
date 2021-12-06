88dcba40acdffdf9228135bebc80135b88ee6d5a
Raf Swiderski <rafswid@icloud.com>
2021-12-06 11:35:46 +0000

    create 3-square board, with test

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -1,9 +1,19 @@
-describe("Player", function() {
+describe("Board", function() {
 
-    it ("should have a default turn value of 0", function() {
-        var board;
+    beforeEach(function(){
         board = new Board();
+    });
+
+    it("should have a default turn value of 0", function() { 
         expect(board.turn).toEqual(0);
     })
 
+    it("should create the details of the board cells", function() { 
+        expect(board.cells).toEqual([
+            { id: "a1", status: "off" },
+            { id: "a2", status: "off" },
+            { id: "a3", status: "off" }
+
+        ]);
+    })
 });
--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,10 @@
 class Board {
     constructor() {
       this.turn = 0;
+      this.cells = [{ id: "a1", status: "off" },
+      { id: "a2", status: "off" },
+      { id: "a3", status: "off" }]
     }
+
+
 }
